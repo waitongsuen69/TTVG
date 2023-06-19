e9e1fb11b01184c34026c2d973dcb421672c6c07
Waitong <waitongsuen@outlook.com>
2023-06-19 22:58:44 +0930

    setup loading dynamic trigger

--- ttvg_client/src/App.js
+++ ttvg_client/src/App.js
@@ -4,25 +4,21 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
 function App() {
+  const [generateLoad, setGenerateLoad] = useState(false);
   const [data, setData] = useState("");
 
-
-  // const handleIMGClick = (event) => {
-  //   fetch("/output_image",{
-  //     method: 'GET',
-      
-  //   })
-  // };
-
   // when click, send `data` to server/user_input and get output, then write output to `result`
-  function handleClick (event){
-    fetch("/user_intput",{
+  async function handleGenerate (event){
+    setGenerateLoad(true);
+
+    await fetch("/user_intput",{
       method: 'POST',
       headers: {'Content-Type':'application/json'},
       body: JSON.stringify(data) // return data from server in json type back to string
     }).then(response => response.json())
     .then(result => {
       //create element of image
+      setGenerateLoad(false);
       const img = document.createElement('img');
       img.src = 'data:image/png;base64,' + result.image;
       document.body.appendChild(img);
@@ -48,18 +44,17 @@ function App() {
         //handle click enter to generate
         onKeyDown={(ev) => {
           if (ev.key === "Enter") {
-            handleClick();
+            handleGenerate();
             ev.preventDefault();
           }
         }}
       />
 
       {/* handle click button to generate */}
-      <Button variant="contained" color="primary" onClick={handleClick}>
+      <Button variant="contained" color="primary" onClick={handleGenerate}>
         Submit
       </Button>
-
-      {typeof data === "undefined" ? <p>Loading...</p> : <p>Done!</p>}
+       {generateLoad ? (<p>loading ...</p>):(<p>Nothing loading ...</p>)}
       
     </div>
   );
